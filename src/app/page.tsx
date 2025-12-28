320247a4287edc59b6406274fa36de1b626e7137
Ella Bektas <ella.bektas@gmail.com>
2025-12-28 12:51:43 +0000

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,26 @@
 
 import styles from "./page.module.css";
 import { Button } from './components/Button/Button'
-import Tabs from './components/Tabs/Tabs';
-import Header from './components/Header/Header';
-import { TabsProps } from '@/app/components/Tabs/Tabs';
 import Link from 'next/link'
 
 
 export default function Home() {
   return (
     <div>
-    
-        <div className={styles.intro}>
+
+      <div className={styles.double}>
+        <div className={styles.container}>
+          <h1 className={styles.h1}>Building great products</h1>
+          <p className={styles.p}>Intro goes here</p>
+
+        </div>
+       
+        <p>Image goes here</p>
+
+      </div>
+
+      <div> 
+      <div className={styles.intro}>
           <h1>Hello, Welcome!</h1>
           <p>
           Hi! I’m a User Experience Designer at Gearset.  I’ve been designing, trying and testing great tech products professionally for over 3 years.
@@ -25,6 +34,15 @@ export default function Home() {
         <Button iconLeft="User" variant="secondary">About</Button>
         </Link>
         </div>
+      </div>
+
+
+      <div className={styles.single}> 
+        <h1>Interested in my work?</h1>
+        <p>Ellabektas.com is my corner of the internet where you can find out all about what I love to do. If you want to read about how I designed and coded this website, checkout the case study here.</p>
+      </div>
+    
+       
         
       
     </div>
