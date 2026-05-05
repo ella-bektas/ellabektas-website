30740e0eaea3b51e62f4a104dec8ff488379d0c8
ella-bektas <ella.bektas@gmail.com>
2026-05-05 18:50:51 +0100

    Styling changes

--- src/app/components/ProjectTile/ProjectTile.tsx
+++ src/app/components/ProjectTile/ProjectTile.tsx
@@ -3,18 +3,21 @@
 import Link from 'next/link';
 import styles from './ProjectTile.module.css';
 
+type Alignment = 'bottom'  | 'center';
+
 type ProjectTileProps = {
   date: string;
   title: string;
   image?: string;
   description: string;
   href: string;
+  alignment?: Alignment;
 };
 
-const ProjectTile = ({ date, title, image, description, href }: ProjectTileProps) => {
+const ProjectTile = ({ date, title, image, description, href, alignment = 'bottom' }: ProjectTileProps) => {
   return (
     <Link href={href} className={styles.container}>
-      <div className={styles.imageContainer}>
+      <div className={`${styles.imageContainer} ${styles[alignment]}`}>
         {image && <img className={styles.image} src={image} alt={title} />}
       </div>
       <div className={styles.date}>{date}</div>
