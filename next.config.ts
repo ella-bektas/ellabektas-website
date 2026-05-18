9ef1aa9cabbc00c2ea7a7965cc002b97032c7bd2
ella-bektas <ella.bektas@gmail.com>
2026-05-18 13:23:01 +0100

    Image path added

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",  // <=== enables static exports
-reactStrictMode: true,
-basePath: "/ellabektas-website", // <=== set the base path to your repository name
+
+  basePath: "/ellabektas-website", // <=== set the base path to your repository name
 };
 
 export default nextConfig;
