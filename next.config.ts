45eb2dc02a9d50c347a7f6bebd5d8f36e97d514d
ella-bektas <ella.bektas@gmail.com>
2026-05-18 13:48:38 +0100

    config removed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
-  basePath: "/ellabektas-website", // <=== set the base path to your repository name
+ 
 };
 
 export default nextConfig;
