e16ece921881b9fe8cd5ad0e7bd10daeafe06ab0
ella-bektas <ella.bektas@gmail.com>
2026-05-18 13:03:04 +0100

    Out folder

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/ellabektas-website', // must match your repo name
+  distDir: 'docs',  // add this line
   images: { unoptimized: true },
   /* config options here */
 };
