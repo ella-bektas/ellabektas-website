f41e72589702d7e74fc20bc911a19845e0ff76f6
ella-bektas <ella.bektas@gmail.com>
2026-05-18 13:11:08 +0100

    Base path added

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+reactStrictMode: true,
+basePath: "/ellabektas-website", // <=== set the base path to your repository name
 };
 
 export default nextConfig;
